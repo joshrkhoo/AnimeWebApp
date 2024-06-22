addf7b5b680e9ed0afd784fd86a685fdda730874
Giganotosaurus <75021575+joshrkhoo@users.noreply.github.com>
2024-06-22 15:44:48 +1000

    saving data to api

--- anime_web_app/src/AnimeScheduler.jsx
+++ anime_web_app/src/AnimeScheduler.jsx
@@ -1,3 +1,4 @@
+import { set } from 'lodash';
 import React, { useState, useEffect } from 'react';
 
 const daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
@@ -12,6 +13,63 @@ const AnimeScheduler = ({ selectedAnimes = [] }) => {
         Saturday: [],
         Sunday: []
     });
+
+
+    const parseScheduleDates = (schedule) => {
+        /*
+        Parse schedule to ensure dates are Date objects
+        
+        We need to convert the dates from strings to Date objects so we can sort them otherwise they will be sorted as strings which will cause an error
+        */
+
+        const newSchedule = {};
+        // Iterate over each day in the schedule
+        for (const day in schedule) {
+            newSchedule[day] = schedule[day].map(anime => ({
+                ...anime,
+                airing_time: new Date(anime.airing_time)
+            }));
+        }
+        return newSchedule;
+    };
+
+    const loadSchedule = async () => {
+        try{
+            const response = await fetch('http://127.0.0.1:5000/getSchedule');
+            const data = await response.json();
+            const parsedData = parseScheduleDates(data);
+            console.log(data);
+            setSchedule(parsedData);
+        }catch(err){
+            console.error('An error occurred while fetching the schedule:', err);
+        }
+    
+    };
+
+    const saveSchedule = async (newSchedule) => {
+        try{
+            await fetch('http://127.0.0.1:5000/saveSchedule', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(schedule),
+            });
+        }catch(err){
+            console.error('An error occurred while saving the schedule:', err);
+        }
+
+    };
+
+    useEffect(() => {
+        loadSchedule();
+    }, []);
+
+    useEffect(() => {
+        saveSchedule();
+    }, [schedule]);
+
+
 
     useEffect(() => {
         console.log('Selected animes in AnimeScheduler:', selectedAnimes);
@@ -54,7 +112,7 @@ const AnimeScheduler = ({ selectedAnimes = [] }) => {
                     console.log('Airing time:', airingTime);
                     
                     // Get the airing date
-                    const airingDay = airingTime.toLocaleDateString('en-US', { weekday: 'long' });
+                    const airingDay = airingTime.toLocaleDateString('en-AU', { weekday: 'long' });
                     console.log('Airing day:', airingDay);
                     
                     // Add the anime to the schedule
@@ -81,16 +139,18 @@ const AnimeScheduler = ({ selectedAnimes = [] }) => {
     return (
         <div className="schedule-container">
             {daysOfWeek.map(day => (
-                <div key={day} className="day-schedule">
-                    <h3>{day}</h3>
-                    <ul>
-                        {schedule[day].map(anime => (
-                            <li key={`${anime.id}-${anime.episode}`}>
-                                <strong>{anime.title.romaji}</strong> - Episode {anime.episode} - {anime.airing_time ? anime.airing_time.toLocaleDateString() + ', ' + anime.airing_time.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true }) : 'Aired'}
-                            </li>
-                        ))}
-                    </ul>
-                </div>
+                schedule[day].length > 0 && (
+                    <div key={day} className="day-schedule">
+                        <h3>{day}</h3>
+                        <ul>
+                            {schedule[day].map(anime => (
+                                <li key={`${anime.id}-${anime.episode}`}>
+                                    <strong>{anime.title.romaji}</strong> - Episode {anime.episode} - {anime.airing_time ? `${anime.airing_time.toLocaleDateString()}, ${anime.airing_time.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })}` : 'Aired'}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                )
             ))}
         </div>
     );
